57ac5b18745d3ef571be5dbfb894f3fd43fdb3de
Aniket Saha <141211011+Aniket-a14@users.noreply.github.com>
2026-02-03 16:19:34 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (22.94%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void solve(vector<int>& candidates, int target, int sum, int idx, vector<int>& temp,unordered_map<int,int>& mp){
-        if(sum==target){
+
+    void solve(vector<int>& candidates, int target, int idx, vector<int>& temp){
+        if(target==0){
             ans.push_back(temp);
             return;
         }
 
         for(int i=idx;i<candidates.size();i++){
-            if (i > idx && candidates[i] == candidates[i - 1]) continue;
+            if(i>idx && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]>target) break;
 
-            if(sum + candidates[i] <= target && mp[candidates[i]]>0){
-                temp.push_back(candidates[i]);
-                mp[candidates[i]]--;
-                solve(candidates,target,sum+candidates[i],i,temp,mp);
-                temp.pop_back();
-                mp[candidates[i]]++;
-            }
+            temp.push_back(candidates[i]);
+            solve(candidates,target-candidates[i],i+1,temp);
+            temp.pop_back();
         }
     }
+
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end()); 
+        sort(candidates.begin(),candidates.end());
         vector<int> temp;
-        unordered_map<int,int> mp;
-        for(int x:candidates) mp[x]++;
-        solve(candidates,target,0,0,temp,mp);
+        solve(candidates,target,0,temp);
         return ans;
     }
 };
