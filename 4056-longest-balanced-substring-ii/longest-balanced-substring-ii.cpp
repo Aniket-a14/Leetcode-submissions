2b1efa207ccc86391a614df0bdf40c549d29d51f
Aniket Saha <141211011+Aniket-a14@users.noreply.github.com>
2026-02-14 16:39:42 +0530

    Time: 402 ms (94.46%) | Memory: 78.5 MB (95.93%) - LeetSync

---
+++ 4056-longest-balanced-substring-ii/longest-balanced-substring-ii.cpp
@@ -0,0 +1,58 @@
+#include <memory_resource>
+pmr::unsynchronized_pool_resource pool;
+class Solution {
+public:
+    static int has1(string& s, int n){
+        int cnt=1, len=1;
+        for(int i=1; i<n; i++){
+            if (s[i]==s[i-1]) len++;
+            else {
+                cnt=max(cnt, len);
+                len=1;
+            }
+        }
+        return max(cnt, len);
+    }
+
+    static constexpr long long bias=1<<18, shift=32;
+    inline static long long pack(int x, int y){
+        return ((long long)(x+bias)<<shift)|(long long)(y+bias);
+    }
+
+    static int longestBalanced(string& s) {
+        const int n=s.size();
+        int ans=has1(s, n);
+
+        pmr::unordered_map<long long, int> ab(&pool), bc(&pool), ca(&pool), abc(&pool);
+        
+        ab.reserve(n), bc.reserve(n), ca.reserve(n), abc.reserve(n);
+        abc[pack(0, 0)]=-1;
+        ab[pack(0, 0)]=-1;
+        bc[pack(0, 0)]=-1; 
+        ca[pack(0, 0)]=-1; 
+    
+        array<int, 3> cnt={0};
+        for(int i=0; i<n; i++){
+            cnt[s[i]-'a']++; 
+            const auto [A, B, C]=cnt;
+
+            long long key=pack(B-A, C-A);
+            if(abc.count(key)) ans=max(ans, i-abc[key]);
+            else abc[key]=i;
+
+            key=pack(A-B, C);
+            if(ab.count(key)) ans = max(ans, i-ab[key]);
+            else ab[key]=i;
+
+            key=pack(B-C, A);
+            if(bc.count(key)) ans=max(ans, i-bc[key]);
+            else bc[key]=i;
+
+            key=pack(C-A, B);
+            if(ca.count(key)) ans=max(ans, i-ca[key]);
+            else ca[key]=i;
+        }
+        
+        return ans;
+    }
+};
