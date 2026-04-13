e24a3824753a77d7faca4662cf5a5ce755fcb253
Aniket Saha <141211011+Aniket-a14@users.noreply.github.com>
2026-04-13 19:29:37 +0530

    Time: 6 ms (65.08%) | Memory: 19 MB (61.36%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int ans = 0;
+
+    void dfs(TreeNode* root, long long sum, int target) {
+        if (!root) return;
+
+        sum += root->val;
+
+        if (sum == target) ans++;
+
+        dfs(root->left, sum, target);
+        dfs(root->right, sum, target);
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+        if (!root) return 0;
+
+        dfs(root, 0, targetSum); 
+        pathSum(root->left, targetSum); 
+        pathSum(root->right, targetSum); 
+
+        return ans;
+    }
+};
