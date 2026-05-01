1459ba14667bb5d518919f76b1e94755618bc3fe
Aniket Saha <141211011+Aniket-a14@users.noreply.github.com>
2026-05-01 11:18:22 +0530

    Time: 3 ms (68.84%) | Memory: 23.2 MB (97.17%) - LeetSync

---
+++ 382-linked-list-random-node/linked-list-random-node.cpp
@@ -0,0 +1,28 @@
+
+
+class Solution {
+private:
+    ListNode* head;
+    
+public:
+    Solution(ListNode* head) {
+        this->head = head;
+        srand(time(0)); 
+    }
+    
+    int getRandom() {
+        int count = 0;
+        int result = 0;
+        ListNode* curr = head;
+        
+        while (curr) {
+            count++;
+            if (rand() % count == 0) {
+                result = curr->val;
+            }
+            curr = curr->next;
+        }
+        
+        return result;
+    }
+};
