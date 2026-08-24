2d029d659cbefab05c2b81849a85d74fddbd7d45
Aniket Saha <141211011+Aniket-a14@users.noreply.github.com>
2026-08-24 09:43:50 +0530

    Time: 99 ms (80.97%) | Memory: 87.3 MB (84.19%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        
+        for(int i=1;i<stones.size();i++){
+            stones[i] = stones[i] + stones[i-1];
+        }
+
+        int ans = stones.back();
+
+        for(int i=stones.size()-2;i>0;i--){
+            ans = max(ans,stones[i]-ans);
+        }
+
+        return ans;
+    }
+};
